4205db308cd917c23e8cbfc94c986e4798d9ea18
zhengchentong <zhengchentong@xdf.cn>
2021-06-30 14:30:59 +0800

    Default Changelist

---
+++ cmake-build-debug/CMakeFiles/cpptesteverything.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpptesteverything.dir/main.cpp.o"
+  "cpptesteverything"
+  "cpptesteverything.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpptesteverything.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cpptesteverything.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpptesteverything.
+# This may be replaced when dependencies are built.
